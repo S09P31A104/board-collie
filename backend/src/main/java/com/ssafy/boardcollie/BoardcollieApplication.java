42710b115957ccba669fb67bcaf80100974f7381
윤경민 <yoon1994kr@naver.com>
2023-10-23 14:57:40 +0900

    env: 프로젝트 생성

---
+++ backend/src/main/java/com/ssafy/boardcollie/BoardcollieApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.boardcollie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardcollieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardcollieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/boardcollie/BoardcollieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.boardcollie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardcollieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
