8e8898d107167415aa2bed4486ebedb8727e402e
sgkim <kimsg64090@gmail.com>
2023-10-24 14:44:56 +0900

    feat: db connection 설정 함수 및 secret.py 추가

--- Crawler/db_util.py
+++ Crawler/db_util.py
@@ -1,2 +1,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
+
+
+# df to mysql
+def save_to_mysql(data, table_name, user, password, endpoint, database, port, if_exists='replace'):
+    df = pd.DataFrame(data)
+    connection_str = f'mysql+pymysql://{user}:{password}@{endpoint}:{port}/{database}'
+    engine = create_engine(connection_str, echo=False)
+    df.to_sql(name=table_name, con=engine, index=False, if_exists=if_exists)
+    print(f"Data has been saved to {database}.{table_name}")
