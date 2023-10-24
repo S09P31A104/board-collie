9c4c84a6c0ea8eb0e9581c3aed4900865aa04c97
허주혁 <heojoohyeok@naver.com>
2023-10-24 13:56:43 +0900

    Fix: File Name Fix

--- frontend/board_collie/src/pages/searchresult/SearchResultPage.tsx
+++ frontend/board_collie/src/pages/searchresult/SearchResultPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import SearchBar from '../../components/SearchBar/SearchBar';
+import SearchBar from '../../components/searchbar/SearchBar';
 import { Divider, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './SearchResultPage.css';
