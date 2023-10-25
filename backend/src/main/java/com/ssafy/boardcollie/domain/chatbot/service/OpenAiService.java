76a25492f5762f3aed73156f3d187a06e73233be
윤경민 <yoon1994kr@naver.com>
2023-10-25 17:53:34 +0900

    feat: redis 환경 구축 및 이전 질문 저장

--- backend/src/main/java/com/ssafy/boardcollie/domain/chatbot/service/OpenAiService.java
+++ backend/src/main/java/com/ssafy/boardcollie/domain/chatbot/service/OpenAiService.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -16,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class OpenAiService {
 
     @Value("${openai.url}/chat/")
@@ -23,6 +27,12 @@ public class OpenAiService {
     @Value("${openai.key}")
     private String OPEN_AI_KEY;
     private final RestTemplate restTemplate = restTemplate();
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String QUEUE_KEY = "prevQuestion";
+    private static final int QUEUE_SIZE = 3;
+    private static final String PROMPT_PREFIX = "보드게임 ";
+    private static final String PROMPT_SUFFIX = "의 플레이 룰에 관한 질문이야: ";
+    private static final String PROMPT_LANGUAGE = "부드러운 말투로 '해요'체로 대답해줘";
 
     public String getCompletion(String prompt) {
         String url = API_ENDPOINT + "completions";
@@ -34,9 +44,17 @@ public class OpenAiService {
         List<Map<String, Object>> messages = new ArrayList<>();
         Map<String, Object> userMessage = new HashMap<>();
 
+        ArrayList<String> prevPrompt = getPrevPrompt();
+        for (String s : prevPrompt) {
+            messages.add(createSystemMap("이전 질문이야: " + s));
+        }
+        saveToRedis(prompt);
+        messages.add(createSystemMap(PROMPT_LANGUAGE));
+        String gameName = "루미큐브";
         userMessage.put("role", "user");
-        userMessage.put("content", prompt);
+        userMessage.put("content", PROMPT_PREFIX + gameName + PROMPT_SUFFIX + prompt);
         messages.add(userMessage);
+
         requestBody.put("messages", messages);
         requestBody.put("model", "gpt-4");
         requestBody.put("temperature", 0.8);
@@ -44,6 +62,7 @@ public class OpenAiService {
         requestBody.put("top_p", 1);
         requestBody.put("frequency_penalty", 0.5);
         requestBody.put("presence_penalty", 0.5);
+
 
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
 
@@ -68,6 +87,26 @@ public class OpenAiService {
         });
         restTemplate.setInterceptors(interceptors);
         return restTemplate;
+    }
+
+    public void saveToRedis(String prompt) {
+        ListOperations<String, String> listOps = redisTemplate.opsForList();
+        listOps.leftPush(QUEUE_KEY, prompt);
+        if (listOps.size(QUEUE_KEY) > QUEUE_SIZE) {
+            listOps.rightPop(QUEUE_KEY);
+        }
+    }
+
+    public ArrayList<String> getPrevPrompt() {
+        ListOperations<String, String> listOps = redisTemplate.opsForList();
+        return new ArrayList<>(listOps.range(QUEUE_KEY, 0, -1));
+    }
+
+    private Map<String, Object> createSystemMap(String content) {
+        Map<String, Object> system = new HashMap<>();
+        system.put("role", "system");
+        system.put("content", content);
+        return system;
     }
 
 }
