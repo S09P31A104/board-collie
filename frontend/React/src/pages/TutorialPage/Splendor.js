80d80136c5ebb782456c14b027379881c6d0ab66
SeungAh-Yoo99 <ysa8497@gmail.com>
2023-10-25 14:46:17 +0900

    feat: 튜토리얼 세팅 페이지 수정 중

--- frontend/React/src/pages/TutorialPage/Splendor.js
+++ frontend/React/src/pages/TutorialPage/Splendor.js
@@ -11,15 +11,18 @@ function Splender() {
 
     const [page, setPage] = useState(0);
     const flow = [
-        // [템플렛 이름, 사진, 타이틀, 내용, 버튼1 내용, 버튼1 화면 번호, 버튼2 내용, 버튼2 화면 번호, 버튼3 내용, 버튼3 화면 번호, 버튼4 내용, 버튼4 화면 번호,]
-        [
-            'TutorialStartComponent',
-            '/Tutorial/Splendor/title.png',
-            '스플렌더',
-            '인원 수별 세팅 방법이 달라집니다.\n인원수를 다시 한번 확인해 주세요.',
-            'START', 1,
-            null, null, null, null, null, null
+        [ // 0: 튜토리얼 시작 페이지
+            'TutorialStartComponent', // 템플릿
+            '/Tutorial/Splendor/title.png', // 타이틀 이미지
+            '스플렌더', // 타이틀
+            '인원 수별 세팅 방법이 달라집니다.\n인원수를 다시 한번 확인해 주세요.', // 추가 메시지
+            'START', 1 // 버튼, 이동 페이지 flow 번호
         ],
+        [
+            // 1: 게임 세팅 페이지
+            'GameSettingComponent', // 템플릿
+            
+        ]
     ]
 
     useEffect(() => {
