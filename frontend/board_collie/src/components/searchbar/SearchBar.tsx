98d0073fb941bd596fb26e181ef0427d6188f511
hyunin3 <hjshimel@gmail.com>
2023-10-31 10:45:52 +0900

    fix: 로비페이지 텍스트 애니메이션 수정, 검색페이지에서 사용종료 버튼 잘 안눌리던 오류 수정

--- frontend/board_collie/src/components/searchbar/SearchBar.tsx
+++ frontend/board_collie/src/components/searchbar/SearchBar.tsx
@@ -1,12 +1,44 @@
 import React, { useState, CSSProperties } from 'react';
-import { TextField, Grid, Button } from '@mui/material';
+import { TextField, Grid, Button, Modal } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { useNavigate } from 'react-router-dom';
+import iconLogo from '../../assets/logo.png'
+import styled from 'styled-components';
 
 interface Props {
   onSearch: (query: string) => void;
   style?: CSSProperties; 
 }
+
+const ModalContent = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 30vw;
+  height: 30vh;
+  padding: 20px;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  text-align: center;
+`;
+
+const ModalLogo = styled.div`
+  background-image: url(${iconLogo});
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 11vw;
+  height: 15vh;
+  margin: auto;
+`;
+
+const ButtonWrapper = styled.div`
+  text-align: center;
+`;
+
+
 
 const SearchBar: React.FC<Props> = ({ onSearch, style }) => {
   const [inputValue, setInputValue] = useState("");
@@ -30,6 +62,20 @@ const SearchBar: React.FC<Props> = ({ onSearch, style }) => {
 
   const handleLogoClick = () => {
     navigate('/main');
+  };
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const endGame = () => {
+    navigate('/');
   };
 
   return (
@@ -91,6 +137,28 @@ const SearchBar: React.FC<Props> = ({ onSearch, style }) => {
           검색
         </Button>
       </Grid>
+      <div
+        onClick={openModal}
+        style={{
+          position: 'absolute', 
+          right: 0, 
+          height: '100px',
+          width: '70px', // 또는 원하는 너비
+          cursor: 'pointer',
+          opacity: 0, 
+        }}
+      />
+        <Modal open={modalOpen} onClose={closeModal}>
+        <ModalContent>
+          <ModalLogo />
+          <h2>이용을 종료하시겠습니까?</h2>
+          <ButtonWrapper>
+            <Button onClick={endGame}>예</Button>
+            <Button onClick={closeModal}>아니오</Button>
+          </ButtonWrapper>
+        </ModalContent>
+      </Modal>
+
     </Grid>
   );
 };
