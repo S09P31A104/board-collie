44c418715e198fcc6db561d48b7b832cab896d02
윤경민 <yoon1994kr@naver.com>
2023-10-31 13:14:49 +0900

    feat: 질문 로그 저장

--- backend/src/main/java/com/ssafy/boardcollie/domain/chatbot/entity/Question.java
+++ backend/src/main/java/com/ssafy/boardcollie/domain/chatbot/entity/Question.java
@@ -6,12 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@Setter
+@Table(name = "question")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Question extends BaseEntity {
 
     @Id
@@ -23,4 +27,16 @@ public class Question extends BaseEntity {
     @Column(name = "game_id")
     private Long gameId;
 
+    @Builder
+    private Question(String question, Long gameId) {
+        this.question = question;
+        this.gameId = gameId;
+    }
+
+    public static Question createQuestion(String question, Long gameId) {
+        return Question.builder()
+                       .question(question)
+                       .gameId(gameId)
+                       .build();
+    }
 }
