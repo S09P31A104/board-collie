9af5193f491cbb81534259fd8649760b25d01c90
hyunin3 <hjshimel@gmail.com>
2023-11-06 13:14:57 +0900

    design: 디테일 페이지 모달 크기 및 폰트 크기 조정

--- frontend/board_collie/src/pages/gamedetail/GameDetailPage.tsx
+++ frontend/board_collie/src/pages/gamedetail/GameDetailPage.tsx
@@ -84,7 +84,7 @@ const GameDetailPage: React.FC = () => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 500,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
@@ -238,10 +238,10 @@ const GameDetailPage: React.FC = () => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ fontSize: '1.5rem' }} >
            {selectedTagName}
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          <Typography id="modal-modal-description" sx={{ mt: 2, fontSize: '1.1rem' }} >
             {selectedTagDescription}
           </Typography>
         </Box>
