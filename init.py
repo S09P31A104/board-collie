476bee85e37970047e68bbff975d54363e00b0d5
sgkim <kimsg64090@gmail.com>
2023-11-13 17:00:02 +0900

    feat: Game 추천 서비스 개발

--- init.py
+++ init.py
@@ -8,101 +8,63 @@ import json
 from scipy.sparse import load_npz
 import pickle
 
-# 데이터가 큰 경우 콘솔에 모두 출력하기 위해 옵션 설정
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-
 
 def get_model_and_tokenizer():
     tokenizer = BertTokenizer.from_pretrained('monologg/kobert')
     model = BertModel.from_pretrained('monologg/kobert')
-    print("Successfully Return Model and Tokenizer")
     return model, tokenizer
 
 
-model, tokenizer = get_model_and_tokenizer()
 
-# 데이터 로드
-games = pd.read_csv('csv/game.csv')
-tags = pd.read_csv('csv/mechanism.csv')
-game_tags = pd.read_csv('csv/game_tag.csv')
-with open('csv/stopwords.txt', 'r', encoding='utf-8') as f:
-    stopwords = [line.strip() for line in f]
+def get_component():
+    # 데이터 로드
+    games = pd.read_csv('csv/game.csv')
+    tags = pd.read_csv('csv/mechanism.csv')
+    game_tags = pd.read_csv('csv/game_tag.csv')
+    with open('csv/stopwords.txt', 'r', encoding='utf-8') as f:
+        stopwords = [line.strip() for line in f]
 
-games['combined_info'] = (games['game_title_kor'] + ' ' +
-                          games['game_detail'] + ' ' +
-                          games['game_evaluation'])
+    games['combined_info'] = (games['game_title_kor'] + ' ' +
+                              games['game_detail'] + ' ' +
+                              games['game_evaluation'])
 
-# 태그 ID와 게임 ID를 기반으로 게임 데이터와 태그 데이터 합치기
-merged = pd.merge(game_tags, tags, on='tag_id', how='left')
+    # 태그 ID와 게임 ID를 기반으로 게임 데이터와 태그 데이터 합치기
+    merged = pd.merge(game_tags, tags, on='tag_id', how='left')
 
-# 병합된 데이터를 'combined_info'에 추가
-for _, row in merged.iterrows():
-    if pd.notnull(row['tag_description']):
-        game_id = row['game_id']
-        tag_info = row['tag_description'].lower()
-        games.loc[games['game_id'] == game_id, 'combined_info'] += ' ' + tag_info
+    # 병합된 데이터를 'combined_info'에 추가
+    for _, row in merged.iterrows():
+        if pd.notnull(row['tag_description']):
+            game_id = row['game_id']
+            tag_info = row['tag_description'].lower()
+            games.loc[games['game_id'] == game_id, 'combined_info'] += ' ' + tag_info
 
+    def load_game_vectors(file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
 
-# TF-IDF 벡터화를 위한 함수 (이전과 동일하게 CPU에서 수행됨)
-def tfidf_vectorize(texts):
-    tfidf_vectorizer = TfidfVectorizer()
-    tfidf_matrix = tfidf_vectorizer.fit_transform(texts)
-    return tfidf_vectorizer, tfidf_matrix
+        # 키 값을 숫자로 변환
+        converted_data = {int(k): np.array(v) for k, v in data.items()}
+        return converted_data
 
+    def load_npy_file(file_path):
+        return np.load(file_path)
 
-# koBERT 임베딩 생성 함수 (GPU 사용)
-def bert_embed(texts, tokenizer, model, device):
-    # 평균 임베딩을 계산하기 위한 함수
-    def mean_pooling(token_embeddings, attention_mask):
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-        sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
-        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-        return sum_embeddings / sum_mask
+    def load_sparse_matrix(file_path):
+        return load_npz(file_path)
 
-    # 텍스트를 토큰화하고 BERT 임베딩 계산 (GPU 사용)
-    encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt', max_length=512).to(device)
-    with torch.no_grad():
-        model_output = model(**encoded_input)
+    def load_pickle_file(file_path):
+        with open(file_path, 'rb') as f:
+            return pickle.load(f)
 
-    # 평균 임베딩을 사용하여 문장 벡터 생성
-    sentence_embeddings = mean_pooling(model_output.last_hidden_state, encoded_input['attention_mask'])
+    game_vectors = load_game_vectors('statics/game_vectors.json')
 
-    # GPU에서 계산된 데이터를 CPU로 이동
-    return sentence_embeddings.cpu().numpy()
+    bert_embeddings = load_npy_file('statics/bert_embeddings.npy')
 
+    tfidf_matrix = load_sparse_matrix('statics/tfidf_matrix.npz')
 
-def load_game_vectors(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+    tfidf_vectorizer = load_pickle_file('statics/tfidf_vectorizer.pkl')
 
-    # 키 값을 숫자로 변환
-    converted_data = {int(k): np.array(v) for k, v in data.items()}
-    return converted_data
-
-
-def load_npy_file(file_path):
-    return np.load(file_path)
-
-
-def load_sparse_matrix(file_path):
-    return load_npz(file_path)
-
-
-def load_pickle_file(file_path):
-    with open(file_path, 'rb') as f:
-        return pickle.load(f)
-
-
-game_vectors = load_game_vectors('statics/game_vectors.json')
-
-bert_embeddings = load_npy_file('statics/bert_embeddings.npy')
-
-tfidf_matrix = load_sparse_matrix('statics/tfidf_matrix.npz')
-
-tfidf_vectorizer = load_pickle_file('statics/tfidf_vectorizer.pkl')
+    return game_vectors, bert_embeddings, tfidf_matrix, tfidf_vectorizer
 
 # # 전처리된 텍스트 데이터 벡터화 (CPU에서 수행)
 # tfidf_vectorizer, tfidf_matrix = tfidf_vectorize(games['combined_info'])
