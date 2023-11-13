e43646c299831559cc8e7721de1610c071fb691f
sgkim <kimsg64090@gmail.com>
2023-11-13 17:12:04 +0900

    feat: ignore 설정 수정

--- secret.py
+++ secret.py
@@ -1,10 +1,3 @@
-# DB_ENDPOINT = "board-collie-db.csldfo2pkeyy.ap-northeast-2.rds.amazonaws.com"
-# DB_PORT = 3306
-# DB_USERNAME = "somin"
-# DB_PASSWORD = "ssafy104!"
-# DB_NAME = "board_collie_db"
-# OPENAI_API_KEY = "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"
-
 DB_ENDPOINT = "localhost"
 DB_PORT = 3306
 DB_USERNAME = "ssafy"
