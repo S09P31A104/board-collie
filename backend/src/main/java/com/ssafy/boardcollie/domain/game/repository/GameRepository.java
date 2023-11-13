b62fbd5097d85bd09307e2d080300fac4c78854e
박소민 <yygs321@naver.com>
2023-11-13 16:03:13 +0900

    feat: 태그 검색 기능 추가

--- backend/src/main/java/com/ssafy/boardcollie/domain/game/repository/GameRepository.java
+++ backend/src/main/java/com/ssafy/boardcollie/domain/game/repository/GameRepository.java
@@ -16,12 +16,6 @@ public interface GameRepository extends JpaRepository<Game, Long> {
     List<Game> findGamesBySearchCriteria(@Param("searchKeyword") String searchKeyword,
             @Param("numberOfPeople") Integer numberOfPeople);
 
-//    @Query("SELECT DISTINCT g FROM Game g " +
-//            "JOIN FETCH g.gameTags gt " +
-//            "JOIN FETCH gt.tag t " +
-//            "WHERE t.tagNameKor = :tagName")
-//    List<Game> findGamesByTagName(@Param("tagName") String tagName);
-
     @Query("SELECT DISTINCT g FROM Game g " +
             "LEFT JOIN FETCH g.gameTags gt " +
             "LEFT JOIN FETCH gt.tag t " +
@@ -30,11 +24,7 @@ public interface GameRepository extends JpaRepository<Game, Long> {
             "WHERE g.id = :gameId")
     Optional<Game> findGameDetailWithTagsAndSimilarGames(@Param("gameId") Long gameId);
 
-//    @Query("SELECT distinct g FROM Game g " +
-//            "JOIN fetch GameTag gt ON g.id = gt.game" +
-//            "JOIN fetch Tag t ON gt.tag= t.id " +
-//            "WHERE t.tagNameKor = :tagName AND "
-//            + ":numberOfPeople IS NULL OR (g.minPlayer <= :numberOfPeople AND g.maxPlayer >= :numberOfPeople)")
+
     @Query("SELECT distinct g FROM Game g JOIN FETCH g.gameTags gt JOIN FETCH gt.tag t " +
         "WHERE g.id IN (SELECT g.id FROM Game g JOIN g.gameTags gt JOIN gt.tag t " +
         "WHERE t.tagNameKor = :tagName AND "+
