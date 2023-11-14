5a8860c564e01b59a4e28a7e4a6b01b704116ccb
박소민 <yygs321@naver.com>
2023-11-14 11:00:05 +0900

    merge: merge rec/develop

--- secret.py
+++ secret.py
@@ -1,6 +1,6 @@
-DB_ENDPOINT = "localhost"
+DB_ENDPOINT = "board-collie-db.csldfo2pkeyy.ap-northeast-2.rds.amazonaws.com"
 DB_PORT = 3306
-DB_USERNAME = "ssafy"
-DB_PASSWORD = "ssafy"
-DB_NAME = "test"
+DB_USERNAME = "somin"
+DB_PASSWORD = "ssafy104!"
+DB_NAME = "board_collie_db"
 OPENAI_API_KEY = "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"
